730fe1d43d14fecc375132bdd24dd48006a7a6af
stanleyowen <69080584+stanleyowen@users.noreply.github.com>
2020-10-16 18:09:33 +0700

    Added files via upload

---
+++ Index.php
@@ -0,0 +1,413 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="Stanley Owen">
+  <title>Stanley Owen</title>
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+  <link href="css/grayscale.min.css" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+ 
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand js-scroll-trigger" href="#page-top">Home</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+		  <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#getstarted">Get Started</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#about">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#projects">Timelines</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="#signup">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Header -->
+  <header class="masthead">
+    <div class="container d-flex h-100 align-items-center">
+      <div class="mx-auto text-center">
+        <h1 class="mx-auto my-0 text-uppercase">Welcome</h1>
+        <h2 class="text-white-50 mx-auto mt-2 mb-5"><b>A Website, created using HTML (Hyper Text Markup Language), CSS (Cascading Style Sheets), JS (Java Script), and PHP (Hypertext Preprocessor).</b></h2>
+        <a href="#getstarted" class="btn btn-primary js-scroll-trigger">Get Started</a>
+        <br><br>
+		<a href="#projects" class="btn btn-primary js-scroll-trigger">Story Timelines</a>
+                <br><br>
+		<a href="#post" class="btn btn-primary js-scroll-trigger">Updates</a>
+		<br><br>
+		<a href="#signup" class="btn btn-primary js-scroll-trigger">Get In Touch & Feedbacks</a>
+      </div>
+    </div>
+  </header>
+
+  <!-- Get Started Section -->
+  <section id="getstarted" class="about-section text-left">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 mx-auto">
+          <h2 class="text-white mb-4">HII EVERYBODY</h2>
+			<font color=white>
+				For the best experience : 
+				<ol><li>Please use your Laptop/Personal Computer <font color=Red>(Optional)</font>
+					<li>Please use the recommended search engine to open this website , such as :
+						<ol><li>Google Chrome <font color=Red>(Recommended)</font>
+							<li>Mozilla Firefox <font color=Red>(Recommended)</font>
+							<li>Internet Explorer <font color=Red>(Recommended)</font>
+							<li>Safari <font color=Red>(Recommended)</font>
+							<li>Ask.com <font color=Red>(Recommended)</font>
+							<li>Bing <font color=Red>(Not Recommended)</font>
+							<li>Yahoo! <font color=Red>(Not Recommended)</font>
+							<li>Yandex, etc <font color=Red>(Not Recommended)</font>
+						</ol>
+				</ol>
+			</font>
+			<br>
+			<center><a href="#about" class="btn btn-primary js-scroll-trigger">Go to Next Pages</a></center>
+			<br><br>
+        </div>
+      </div>
+      <img src="img/ipad1.png" class="img-fluid" alt="">
+    </div>
+  </section>
+  
+  <!-- About Section -->
+  <section id="about" class="container">
+	<div class="row">
+		<center>
+		<img src="img/ipad2.png" class="img-fluid" alt="">
+		</center>
+		<br>
+		<div class="masthead2">
+			<div class="row justify-content-center no-gutters">
+				<div class="col-lg-6">
+					<img class="img-fluid" src="img/Aboutme.jpg" alt="">
+					<br><center><font color="white">This picture is taken when I was playing a role a gangster.
+					<br>Taken on 15<sup>th</sup> March 2020</font></center>
+				</div>
+
+				<div class="col-lg-6 order-lg-first">
+					<div class="bg-black text-center h-100 project">
+						<div class="d-flex h-100">
+							<div class="project-text w-100 my-auto text-center text-lg-left">
+								<h2 class="text-white mb-4">HII EVERYBODY</h2>
+								<p class="mb-0 text-white-50">
+									<b>My name is Stanley Owen, you can call me SO too. I studied in 
+									<a href = "https://sutomo-mdn.sch.id/">SMP Sutomo 1 School, Medan, North Sumatera, Indonesia</a>. 
+									I am in 9<sup>th</sup> grade this year. Emm... actually I'm not really good in programming, but I programming is a part of my hobby. 
+									I love computer programming since I was 11 years old. Don't be mind cause this is the first time I create this website :)).</b>
+								</p>
+								<hr class="d-none d-lg-block mb-0 mr-0">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+  </section>
+  
+  <!-- Projects Section -->
+  <section id="projects" class="container projects-section bg-light">
+  
+	<h1><font face="Adobe Caslon Pro Bold"><b>MY STORY TIMELINES</b></font></h1>
+	
+	<div class="container">
+
+	  <!-- Project One Row -->
+	  <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+		<div class="col-lg-6">
+		  <img class="img-fluid" src="img/Office - Story Timeline.jpg" alt="">
+		</div>
+		<div class="col-lg-6">
+		  <div class="bg-black text-center h-100 project">
+			<div class="d-flex h-100">
+			  <div class="project-text w-100 my-auto text-left text-lg-left">
+				<h4 class="text-white"><b>2017~2018 - Microsoft Office</b></h4>
+				<p class="mb-0 text-white-50">
+					In <b>2017</b>, this is the first time I learned <b>computer</b>, which is <b><font color="green">Microsoft Office</font></b>. 
+					learning <b><font color="green">Microsoft Office</font></b> is the first step to master programming. The most challenging part 
+					in Microsoft Office is <b><font color="green">Microsoft Excel</font></b> and <b><font color="green">Microsoft Access</font></b>.
+					<br><br><a href="https://en.wikipedia.org/wiki/Microsoft_Office">What is Microsoft Ofiice ?</a>
+				</p>
+				<hr class="d-none d-lg-block mb-0 ml-0">
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+
+	  <!-- Project Two Row -->
+	  <div class="row justify-content-center no-gutters">
+		<div class="col-lg-6">
+		  <img class="img-fluid" src="img/HTML - Story Timeline.jpg" alt="">
+		</div>
+		<div class="col-lg-6 order-lg-first">
+		  <div class="bg-black text-center h-100 project">
+			<div class="d-flex h-100">
+			  <div class="project-text w-100 my-auto text-right text-lg-right">
+				<h4 class="text-white"><b>2018 - HTML (Hyper Text Markup Language)</b></h4>
+				<p class="mb-0 text-white-50">
+					After 1 year of learning <b>Microsoft Office</b>, I started to learn <b><font color="green">HTML language</font></b>. 
+					<b>HTML</b> is the acronym for <b><i>Hyper Text Markup Language</i></b>. It teaches you how to <b>create</b> 
+					<u>a very simply website to a complicated websites</u>. The advantage from HTML coding is 
+					it is very easy to understand.
+					<br><br><a href="https://www.google.com/search?safe=strict&sxsrf=ALeKk00Gx1jSPVToSU77oVfeFYQHQOsCvA%3A1588663723027&ei=qxWxXpOhAZWprtoPgu-byAM&q=What+is+HTML+%3F&oq=What+is+HTML+%3F&gs_lcp=CgZwc3ktYWIQAzIECCMQJzIFCAAQkQIyBQgAEJECMgIIADIECAAQCjICCAAyAggAMgIIADICCAAyBwgAEBQQhwI6BAgAEEc6BAgAEEM6BQgAEMsBOgUIABCDAVCBjwFYjb4BYIfEAWgAcAN4AIABiwKIAesOkgEGMC4xMy4xmAEAoAEBqgEHZ3dzLXdpeg&sclient=psy-ab&ved=0ahUKEwjTooPHmZzpAhWVlEsFHYL3BjkQ4dUDCAw&uact=5">
+					What is HTML ?</a>
+				</p>
+				<hr class="d-none d-lg-block mb-0 mr-0">
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+
+	  <!-- Project Three Row -->
+	  <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+		<div class="col-lg-6">
+		  <img class="img-fluid" src="img/CF - Story Timeline.jpg" alt="">
+		</div>
+		<div class="col-lg-6">
+		  <div class="bg-black text-center h-100 project">
+			<div class="d-flex h-100">
+			  <div class="project-text w-100 my-auto text-left text-lg-left">
+				<h4 class="text-white"><b>2018~2019 - Computer Fundamental</b></h4>
+				<p class="mb-0 text-white-50">
+					In <b>2018</b>, I learned something interesting from <i>school</i>, which is <b><font color="green">Computer 
+					Fundamental</font></b>. Fundamental means "<i>basic</i>", so Computer Fundamental is the basic learning of computer. 
+					It teaches us from <b>how a computer being created</b> till <b>how to protect your computer from virus, web phishing, etc</b>.
+					<br><br><a href="https://www.britannica.com/technology/computer">What is Computer Fundamental ?</a>
+				</p>
+				<hr class="d-none d-lg-block mb-0 ml-0">
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	  
+	  <!-- Project Four Row -->
+	  <div class="row justify-content-center no-gutters">
+		<div class="col-lg-6">
+		  <img class="img-fluid" src="img/C++ - Story Timeline.jpg" alt="">
+		</div>
+		<div class="col-lg-6 order-lg-first">
+		  <div class="bg-black text-center h-100 project">
+			<div class="d-flex h-100">
+			  <div class="project-text w-100 my-auto text-right text-lg-right">
+				<h4 class="text-white"><b>2019 - C++</b></h4>
+				<p class="mb-0 text-white-50">
+					While I was learning Computer Fundamental, I started to learn <b>something different</b> than 
+					before (design a web). It was <b><font color="green">C++</font></b>, it's classified to <b>C language</b>. It was very different 
+					than what I had ever learnt before, starting from its syntax, intrepeter, etc. Although 
+					it was quite different from the one I have ever learnt before, but I enjoyed it.
+					<br><br><a href="https://en.wikipedia.org/wiki/C%2B%2B">What is C++ ?</a>
+				</p>
+				<hr class="d-none d-lg-block mb-0 mr-0">
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>	  
+	  
+	  <!-- Project Five Row -->
+	  <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+		<div class="col-lg-6">
+		  <img class="img-fluid" src="img/Java - Story Timeline.png" alt="">
+		</div>
+		<div class="col-lg-6">
+		  <div class="bg-black text-center h-100 project">
+			<div class="d-flex h-100">
+			  <div class="project-text w-100 my-auto text-left text-lg-left">
+				<h4 class="text-white"><b>2019 - Java</b></h4>
+				<p class="mb-0 text-white-50">
+					After finished lerning <b>C++</b>, I took the another language, <b>Java</b>. <b><font color="green">Java language</font></b> is very popular nowadays. 
+					This language for me is <i>very challenging and difficult</i>, because its really need <b>logic</b> and you need to 
+					be very careful to type the code, such as <i><font color="orange">System.out.println("Hello, World")</font></i>. Mistyping such as 
+					caps lock will cause <b><font color="red">error</font></b>. But from this language, I get to know that, learning every programming languages 
+					need <b>PATIENT</b>.
+					<br><br><a href="https://java.com/en/download/faq/whatis_java.xml">What is Java Programming Language ?</a>
+				</p>
+				<hr class="d-none d-lg-block mb-0 ml-0">
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+
+	  <!-- Project Six Row -->
+	  <div class="row justify-content-center no-gutters">
+		<div class="col-lg-6">
+		  <img class="img-fluid" src="img/CSS - Story Timeline.jpg" alt="">
+		</div>
+		<div class="col-lg-6 order-lg-first">
+		  <div class="bg-black text-center h-100 project">
+			<div class="d-flex h-100">
+			  <div class="project-text w-100 my-auto text-right text-lg-right">
+				<h4 class="text-white"><b>2020~Now - CSS (Cascade Style Sheets) + PHP (Hypertext Preprocessor)</b></h4>
+				<p class="mb-0 text-white-50">
+					Today, <i>05 Mei 2020</i>, I am studying at home right now <b>(COVID-19)</b>. Because of boring, I try to learn something new :) 
+					<b><font color="green">CSS (Cascading Style Sheets) and PHP (Hypertext Preprocessor)</font></b> is almost same as HTML, but it is more difficult than HTML. But it will be <i>easier</i> if you have 
+					ever learnt HTML.
+					<br><br><a href="https://www.google.com/search?safe=strict&sxsrf=ALeKk02zn6oAhBcNxVyXBcjEZereWNykAQ%3A1588667527060&ei=hySxXoGyA4qJ4-EP4p2h-As&q=What+is+CSS+%3F&oq=What+is+CSS+%3F&gs_lcp=CgZwc3ktYWIQAzICCAAyAggAMgIIADICCAAyAggAMgIIADIFCAAQkQIyBQgAEJECMgIIADICCAA6BAgjECc6BQgAEIMBOgUIABDLAToHCAAQFBCHAlD9O1jKWWCsW2gAcAB4AIABrwGIAYgOkgEEMC4xM5gBAKABAaoBB2d3cy13aXo&sclient=psy-ab&ved=0ahUKEwjBkffcp5zpAhWKxDgGHeJOCL8Q4dUDCAw&uact=5">
+					What is CSS (Cascade Style Language) ?</a>
+					<br><a href="https://www.php.net/manual/en/intro-whatis.php">
+					What is PHP (Hypertext Preprocessor) ?</a>
+				</p>
+				<hr class="d-none d-lg-block mb-0 mr-0">
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>		  
+	  
+	  <!-- Project Seven Row -->
+	  <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+		<div class="col-lg-6">
+		  <img class="img-fluid" src="img/Python - Story Timeline.png" alt="">
+		</div>
+		<div class="col-lg-6">
+		  <div class="bg-black text-center h-100 project">
+			<div class="d-flex h-100">
+			  <div class="project-text w-100 my-auto text-left text-lg-left">
+				<h4 class="text-white"><b>2020~Now - Python</b></h4>
+				<p class="mb-0 text-white-50">
+					Learning <b><font color="green">Python</font></b> isn't really difficult as Java and it is fun.
+					<br>What makes it different from another languages are :
+					<ul><Font color="Gray">
+						<li>Syntax are easier to understand
+						<li>You could create an animation using <b><font color="orange">*import turtle</font></b>, etc
+					</ul></font>
+					<a href="https://www.python.org/doc/essays/blurb/">What is Python Programming ?</a>
+				</p>
+				<hr class="d-none d-lg-block mb-0 ml-0">
+			  </div>
+			</div>
+		  </div>
+		</div>
+	  </div>
+	</div>
+  </section>
+  
+  <!-- Background of Background -->
+  <section id="signup" class="transition">
+	  <!-- Signup Section -->
+	  <section id="signup1" class="signup-section">
+		<div class="container">
+		  <div class="row">
+			<div class="col-md-10 col-lg-8 mx-auto text-center">
+			  <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
+			  <h2 class="text-white mb-3">STAY UPDATED<br> & <br> GIVE FEEDBACK</h2>
+			  <a href="/form/stayupdated.html" class="btn btn-primary js-scroll-trigger">STAY UPDATED</a>
+			  <a href="/form/feedback.html" class="btn btn-primary js-scroll-trigger">FEEDBACK</a>
+			</div>
+		  </div>
+		</div>
+	  </section>
+	  
+	  <!-- Contact Section -->
+	  <section id="contact" class="contact-section bg-black">
+		<div class="container">
+		  <div class="row">
+			<div class="col-md-4 mb-3 mb-md-0">
+			  <div class="card py-4 h-100">
+				<div class="card-body text-center">
+				  <i class="fas fa-map-marked-alt text-primary mb-2"></i>
+				  <h4 class="text-uppercase m-0">School Address</h4>
+				  <hr class="my-4">
+				  <div class="small text-black-50">
+					<a href="https://goo.gl/maps/boPxo5NViy2HHMta8">SMP SUTOMO 1 MEDAN<br>Jl. DR. F.L. Tobing No.15, RT.02, Pusat Ps., Kec. Medan Kota, Kota Medan, Sumatera Utara 20232</a>
+				  </div>
+				</div>
+			  </div>
+			</div>
+
+			<div class="col-md-4 mb-3 mb-md-0">
+			  <div class="card py-4 h-100">
+				<div class="card-body text-center">
+				  <i class="fas fa-envelope text-primary mb-2"></i>
+				  <h4 class="text-uppercase m-0">Email</h4>
+				  <hr class="my-4">
+				  <div class="medium text-black-50">
+					<a href="mailto:stanleyowennn@gmail.com">stanleyowennn@gmail.com</a>
+				  </div>
+				</div>
+			  </div>
+			</div>
+
+			<div class="col-md-4 mb-3 mb-md-0">
+			  <div class="card py-4 h-100">
+				<div class="card-body text-center">
+				  <i class="fas fa-mobile-alt text-primary mb-2"></i>
+				  <h4 class="text-uppercase m-0">Phone<br>(Whatsapp)</h4>
+				  <hr class="my-4">
+				  <div class="medium text-black-50">
+					<a href="https://api.whatsapp.com/send?phone=+6281396590955">(+62) 81396590955</a>
+				  </div>
+				</div>
+			  </div>
+			</div>
+		  </div><br>
+		  <b><font color=White>My Masterpiece :</b>
+		  <ul>
+			<li><a href="http://stanleyowen.myartsonline.com">stanleyowen.myartsonline.com</a>
+			<li><a href="http://theeternity.atwebpages.com">theeternity.atwebpages.com</a>
+			<li><a href="http://eternitycouncil.atwebpages.com">eternitycouncil.atwebpages.com</a>
+			<li><a href="http://eternitygc.atwebpages.com">eternitygc.atwebpages.com</a>
+		  <br><br><br><center><font color=White>Share this website :</font></center>
+		  
+		  <div class="social d-flex justify-content-center">
+		
+			<a href="whatsapp://send?text=http%3A%2F%2Fstanleyowen.myartsonline.com%2F" data-action="share/whatsapp/share" class="mx-2">
+			  <i class="fab fa-whatsapp"></i>
+			</a>
+			<a href="https://web.facebook.com/sharer.php?u=http%3A%2F%2Fstanleyowen.myartsonline.com%2F%3D&_rdc=1&_rdr" class="mx-2">
+			  <i class="fab fa-facebook"></i>
+			</a>
+			<a href="https://twitter.com/intent/tweet?url=http%3A%2F%2Fstanleyowen.myartsonline.com%2F&text=A+website%2C+created+by+using+HTML+CSS+Java+Script+and+PHP." class="mx-2">
+			  <i class="fab fa-twitter"></i>
+			</a>
+			<a href="http://pinterest.com/pin/create/button/?url=https%3A%2F%2Fstanleyowen.myartsonline.com&description=A+website%2C+created+by+using+HTML+CSS+Java+Script+and+PHP." class="mx-2">
+			  <i class="fab fa-pinterest"></i>
+			</a>
+		  </div>
+		</div>
+	  </section>
+
+	  <!-- Footer -->
+	  <footer class="bg-black small text-center text-white-50">
+		<div class="container">
+		  Copyright &copy; 2020 Stanley Owen - All rights reserved.
+		</div>
+	  </footer>
+	</section>
+  
+  <!-- Java Script -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="js/grayscale.min.js"></script>
+
+</body>
+
+</html>
