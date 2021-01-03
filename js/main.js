764f35202b802ae31f05fcff5602d102eb91ad9b
stanleyowen <69080584+stanleyowen@users.noreply.github.com>
2021-01-03 19:20:24 +0700

    Fixed Service Worker

--- js/main.js
+++ js/main.js
@@ -4,16 +4,17 @@ var scrollTop_btn = document.getElementById("scrollTop");
 var btnMode1 = document.querySelector(".btn__changeMode");
 var currentTheme = localStorage.getItem("theme");
 var form = document.getElementById('comment__form');
+
 if (window.attachEvent) {
     observe = function (element, event, handler) {
         element.attachEvent('on'+event, handler);
     };
-}
-else {
+}else {
     observe = function (element, event, handler) {
         element.addEventListener(event, handler, false);
     };
 }
+
 function init() {
     function resize () {
         form.style.height = 'auto';
@@ -31,14 +32,17 @@ function init() {
     form.select();
     resize();
 }
+
 document.addEventListener("DOMContentLoaded", function(){
     setTimeout(function(){
         document.body.classList.add('loaded');
     }, 500);
 });
+
 if(currentTheme == "dark"){
     document.body.classList.add("dark");
 }
+
 btnMode1.addEventListener("click", function(){
     document.body.classList.toggle("dark");
     let theme = "light";
@@ -47,7 +51,9 @@ btnMode1.addEventListener("click", function(){
     }
     localStorage.setItem("theme", theme);
 })
+
 window.onscroll = function() {scrollButton()};
+
 function scrollButton() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     scrollTop_btn.style.display = "block";
@@ -55,10 +61,12 @@ function scrollButton() {
     scrollTop_btn.style.display = "none";
   }
 }
+
 function scrollToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
 function toggleNavbar() {
     var menu = document.getElementById("navbar__menu");
     if(menu.style.display === "block"){
@@ -67,10 +75,11 @@ function toggleNavbar() {
         menu.style.display = "block";
     }
 }
+
 $('a[href*=\\#]').on('click', function(event){
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
 });
-window.onscroll = function() {scrollButton()};
+
 $(window).scroll(function(){
     $('.content__fadeIn').each(function(){
         var topElement = $(this).offset().top;
@@ -86,12 +95,3 @@ $(window).scroll(function(){
 setTimeout(function(){
     $('.content__transition').fadeIn(1500);  
 }, 1000);
-
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('https://stanleyowen.github.io/js/sw.min.js')
-    .then(function() {
-      console.log('Service Worker Complete');
-    }, function() {
-      console.log('Error: Service Worker Failure');
-    });
-}
